3233cff8f3a2a41f5cc9bebddf3fb42d3effe5e4
Vaidehi Shrivastava <vaidehishrivastava09@gmail.com>
2024-02-26 16:13:34 +0530

    BrowserRouter and Routes configured

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Landing from './pages/Landing';
+import Cart from './pages/Cart';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Landing/>}></Route>
+        <Route path="/Cart" element={<Cart/>}></Route>
+      </Routes>
+
     </div>
   );
 }
